c9fa00fec148533b33b91322336781546fd56ed9
Kykymar <gold_owl96@mail.ru>
2021-06-11 19:20:10 +0300

    SecondTask

---
+++ SecondTask.java
@@ -0,0 +1,10 @@
+package ru.geekbrains.lesson1.FirstHW.git;
+
+public class SecondClass {
+    public static void methodName();}
+{public static int summ(int a, int b) {
+    if (a+b>=10 & a+b<=20) {
+        System.out.println("true")
+    }
+}
+}
